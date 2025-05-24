3a6f45bfd4e98f11e28b1fe713d8660ec3da6b4f
Lebron Rumeral <lebronrumeral@gmail.com>
2025-05-24 13:17:55 +0800

    test

--- api/connect.js
+++ api/connect.js
@@ -20,10 +20,10 @@ console.log(`Database: ${process.env.DB_NAME}`);
 
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_NAME
 };
 
 export const db = mysql.createPool(dbConfig);
