73b8ac6c9e14597fd2e1559576cb7c49797067c4
Lebron Rumeral <lebronrumeral@gmail.com>
2025-05-24 13:13:33 +0800

    connection

--- api/connect.js
+++ api/connect.js
@@ -23,10 +23,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.DB_PORT || 3306,
 };
 
 export const db = mysql.createPool(dbConfig);
