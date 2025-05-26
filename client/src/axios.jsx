c29934338723270fa43bd7325d1bacd19f877c5c
Lebron Rumeral <lebronrumeral@gmail.com>
2025-05-26 15:54:55 +0800

    TEST

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://gordonconnect-production-f2bd.up.railway.app/api",
+  baseURL: "http://gordonconnect-production-f2bd.up.railway.app/api",
   withCredentials: true,
 });
