36ccaee2ea97eaba4442d76194b6d79329e4fd81
Lebron Rumeral <lebronrumeral@gmail.com>
2025-05-26 16:52:55 +0800

    AXIOS

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -1,6 +1,11 @@
+// axios.js or wherever you configure axios
 import axios from "axios";
+
+const token = localStorage.getItem("token");
 
 export const makeRequest = axios.create({
   baseURL: "https://gordonconnect-production-f2bd.up.railway.app/api",
-  withCredentials: true,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : undefined,
+  },
 });
