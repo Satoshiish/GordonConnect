d106479c650a4060c6602e3b88e5fc70d6a2afcc
Lebron Rumeral <lebronrumeral@gmail.com>
2025-05-26 09:30:06 +0800

    TEST

--- api/connect.js
+++ api/connect.js
@@ -1,10 +1,14 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "Localhost1234",
-  database: "social",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 };
 
 export let db;
@@ -15,7 +19,7 @@ const handleDisconnect = () => {
   db.connect((err) => {
     if (err) {
       console.error("❌ Database connection failed:", err);
-      setTimeout(handleDisconnect, 2000); // Try reconnecting after 2 seconds
+      setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
     } else {
       console.log("✅ Connected to MySQL database.");
     }
@@ -32,4 +36,4 @@ const handleDisconnect = () => {
   });
 };
 
-handleDisconnect(); // Initialize connection
+handleDisconnect();
